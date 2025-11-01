4c92656ceeda4761fccb67888165053733553966
KaynHousen <jimmyantoni15@gmail.com>
2025-11-01 16:48:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.getElementById("form").addEventListener("submit", function(e){
   e.preventDefault();
   const nombre = document.getElementById("nombre").value;
-  document.getElementById("respuesta").textContent = `Gracias por tu mensaje, ${nombre}!`;
+  document.getElementById("respuesta").textContent = `Gracias por tu mensaje, ${nombre}! 💪`;
   this.reset();
+});
+
+document.getElementById("btn-empezar").addEventListener("click", () => {
+  window.scrollTo({
+    top: document.getElementById("casa").offsetTop,
+    behavior: 'smooth'
+  });
 });
